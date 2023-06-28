b1c9f9bd1d5dd638f8c59eb07d718a2563b6988d
smilitelli <sandramilitelli@gmail.com>
2023-06-28 00:33:46 -0300

    Mostrando cards con name, username y id

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Card = ({ name, username, id }) => {
+const Card = ({ props }) => {
 
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
@@ -10,6 +10,9 @@ const Card = ({ name, username, id }) => {
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
+        <h3>{props.name}</h3>
+        <h4>{props.username}</h4>
+        <p>{props.id}</p>
 
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Components/Card'
+import axios from 'axios'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const [cards, setCards] = useState([]) 
+  
+  const url = "https://jsonplaceholder.typicode.com/users"
+
+  useEffect(() => {
+     axios(url)
+     .then(response => setCards(response.data))
+  }, [])
+
   return (
     <main className="" >
       <h1>Home</h1>
       <div className='card-grid'>
         {/* Aqui deberias renderizar las cards */}
+        <ul>    
+          {cards ?
+            cards.map((props) => (
+              <li key={props.id}>
+                <Card props={props} />             
+              </li>
+)) : null}
+            </ul>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
